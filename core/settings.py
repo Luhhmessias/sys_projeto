619755a52df80001c13f8ca0bf60a07c224db3b5
dani <danielaolly@yahoo.com.br>
2026-02-09 11:22:17 -0300

    atualização do debug

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no&wzzb&f%l4r8u+(=u=vb!!^-=brul01h##%88%!ge_&mga&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [daniellaollly.pythonanywhere.com]
 
